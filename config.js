8116abcb851cda1c664939cd53d5276bdc571fca
Alberto Gaytán <98986762+ypsok@users.noreply.github.com>
2026-02-19 21:03:46 -0600

    Refactor config.js and update token encryption
    Refactor CONFIG and caesarShift function, and update token assignment.

--- config.js
+++ config.js
@@ -3,35 +3,24 @@ const CONFIG = {
     repo: "Eph.TDL.Parser",
     branch: "main",
     path: "online-tasks.csv",
-
     token: "bck_BcfPNmVzSLjYPga7bUuJaKJMDV7kgB0esXtU"
 };
-
-const REAL_TOKEN = caesarShift(CONFIG.token, 5);
 
 function caesarShift(str, shift) {
     return str.split("").map(c => {
 
         const code = c.charCodeAt(0);
 
-        // A-Z
-        if (code >= 65 && code <= 90) {
-            return String.fromCharCode(
-                ((code - 65 + shift + 26) % 26) + 65
-            );
-        }
+        if (code >= 65 && code <= 90)
+            return String.fromCharCode(((code - 65 + shift + 26) % 26) + 65);
 
-        // a-z
-        if (code >= 97 && code <= 122) {
-            return String.fromCharCode(
-                ((code - 97 + shift + 26) % 26) + 97
-            );
-        }
+        if (code >= 97 && code <= 122)
+            return String.fromCharCode(((code - 97 + shift + 26) % 26) + 97);
 
-        // números y símbolos quedan igual
         return c;
 
     }).join("");
 }
 
-
+// 🔥 Sobrescribe el token directamente
+CONFIG.token = caesarShift(CONFIG.token, 5);
