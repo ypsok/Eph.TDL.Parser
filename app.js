244dcc7033f10a72a6fa9e514782cb3d8735550d
Alberto Gaytán <98986762+ypsok@users.noreply.github.com>
2026-02-19 20:57:05 -0600

    Refactor task creation functions and add caesarShift

--- app.js
+++ app.js
@@ -1,3 +1,28 @@
+function caesarShift(str, shift) {
+    return str.split("").map(c => {
+
+        const code = c.charCodeAt(0);
+
+        // A-Z
+        if (code >= 65 && code <= 90) {
+            return String.fromCharCode(
+                ((code - 65 + shift + 26) % 26) + 65
+            );
+        }
+
+        // a-z
+        if (code >= 97 && code <= 122) {
+            return String.fromCharCode(
+                ((code - 97 + shift + 26) % 26) + 97
+            );
+        }
+
+        // números y símbolos quedan igual
+        return c;
+
+    }).join("");
+}
+
 function statusColor(status) {
     switch (status) {
         case "En proceso": return "gold";
@@ -52,7 +77,8 @@ async function fetchFile() {
 
     const res = await fetch(url, {
         headers: {
-            Authorization: `token ${CONFIG.token}`
+            Authorization: `token ${REAL_TOKEN}`
+
         }
     });
 
@@ -79,9 +105,10 @@ async function pushFile(content) {
     await fetch(url, {
         method: "PUT",
         headers: {
-            Authorization: `token ${CONFIG.token}`,
+            Authorization: `token ${REAL_TOKEN}`,
             "Content-Type": "application/json"
         },
         body: JSON.stringify(body)
     });
 }
+
