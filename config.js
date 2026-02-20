748e830e890d2d9d36cba30954cb1fc2e08b4f05
Alberto Gaytán <98986762+ypsok@users.noreply.github.com>
2026-02-19 21:00:31 -0600

    Implement caesarShift function for token encryption
    Added caesarShift function to encrypt the token.

--- config.js
+++ config.js
@@ -8,3 +8,30 @@ const CONFIG = {
 };
 
 const REAL_TOKEN = caesarShift(CONFIG.token, 5);
+
+function caesarShift(str, shift) {
+    return str.split("").map(c => {
+
+        const code = c.charCodeAt(0);
+
+        // A-Z
+        if (code >= 65 && code <= 90) {
+            return String.fromCharCode(
+                ((code - 65 + shift + 26) % 26) + 65
+            );
+        }
+
+        // a-z
+        if (code >= 97 && code <= 122) {
+            return String.fromCharCode(
+                ((code - 97 + shift + 26) % 26) + 97
+            );
+        }
+
+        // números y símbolos quedan igual
+        return c;
+
+    }).join("");
+}
+
+
